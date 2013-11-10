d614a9a8a61bb8101fda132a07eb0dbd6a6ef7ee
4l1fe <4l1fe@users.noreply.github.com>
2013-11-10 20:38:00 +0400

    add room change effect

--- chat_app/urls.py
+++ chat_app/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('chat_app.views',
     url(r'^news/$', 'main'),
@@ -7,6 +8,5 @@ urlpatterns = patterns('chat_app.views',
     url(r'^pedia/main/about/', 'main'),
     url(r'^pedia/main/rules/', 'main'),
     url(r'^$', 'main'),
-
-    url(r'^rooms/(.+)/', 'room'),
+    url(r'^show_tabs/', TemplateView.as_view(template_name='tabs.html')),
 )
