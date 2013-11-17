5034995c7aef17e1a85090e9118a9d0c0c5d613f
4l1fe <4l1fe@users.noreply.github.com>
2013-11-17 19:41:04 +0400

    удаление комнат не релизовано редактирование надо допилить

--- chat_app/tornadoapp.py
+++ chat_app/tornadoapp.py
@@ -9,6 +9,7 @@ import re
 import json
 from .models import Room, Message, CustomUser
 from django.contrib.sessions.models import Session
+from django.core.exceptions import ObjectDoesNotExist
 
 
 logging_handler = logging.FileHandler(filename='chat_debug_log.txt', mode='a')
@@ -19,7 +20,7 @@ logger.addHandler(logging_handler)
 logger.setLevel(logging.INFO)
 
 #filename = os.path.realpath('badwords.txt')
-with open(r'D:\SCRIPTS\DJANGO\tornado_chat\chat_app\badwords.txt') as file:
+with open(r'D:\SCRIPTS\DJANGO\tornadochat\chat_app\badwords.txt') as file:
     badwords = [line.decode('cp1251').strip() for line in file.readlines()]
 #logger.info(badwords)
 
@@ -48,107 +49,71 @@ class ChatSocketHandler(tornado.websocket.WebSocketHandler):
         self.connections[first_room].append(self) # добавляю своё соединение в общий словарь.
 
         users_in_f_room = [con.user_name for con in self.connections[first_room]]
-        d = {'text': 'initialization',
+        d = {'type': 'initialization',
              'rooms': [r.encode() for r in self.room_names],
              'users': users_in_f_room}
         mess = json.dumps(d)
         self.write_message(mess)  # шлю себе начальные данные(комнаты, пользователей в 1й комнате)
 
-        #for user_in_f_room, connection in self.connections[first_room].items():
-        #    if user_in_f_room != self.user_name:  # уведомление в чат всем , кроме себя
-        #        d = {'room': first_room,
-        #             'text': 'new_user',
-        #             'user': user_in_f_room}
-        #        mess = json.dumps(d)
-        #        connection.write_message(mess)
-
         for connection in self.connections[first_room]:
             if connection.user_name != self.user_name:  # уведомление в чат всем , кроме себя
                 d = {'room': first_room,
-                     'text': 'new_user',
+                     'type': 'add_user',
                      'user': connection.user_name}
                 mess = json.dumps(d)
                 connection.write_message(mess)
 
-    #def chat_handler(self, message):
-    #    if message['text'] == 'disconnect':
-    #        d = {'room': message['room'],
-    #             'text': 'remove_user',
-    #             'user': self.user_name}
-    #        mess = json.dumps(d)
-    #        all_connections = [con for k in self.connections for con in self.connections[k].values()]
-    #        for con in all_connections:
-    #            con.write_message(mess)
-    #        del self.connections[message['room']][self.user_name]
-    #    elif message['text'] == 'change_room':
-    #        logger.info('pre_change = '+self.connections)
-    #        self.connections[message['new_room']][self.user_name] = self
-    #        logger.info('post_change = '+self.connections)
-    #        self.connections[message['room']].pop(self.user_name)  # тут чё та валится постоянно
-    #        logger.info('post_pop = '+self.connections)
-    #    else:
-    #        room = Room.objects.get(title=message['room'])
-    #        message_obj = Message(room=room, username=self.user_name, text=message['text'])
-    #        message_obj.save()
-    #        d = {'room': message['room'],
-    #             'user': self.user_name,
-    #             'text': censor_message_text(message['text'])}
-    #        mess = json.dumps(d)
-    #        all_connections = [con for k in self.connections for con in self.connections[k].values()]
-    #        logger.info('all_coon = '+str(all_connections))
-    #        for con in all_connections:
-    #            con.write_message(mess)
-
     def chat_handler(self, message):
-        if message['text'] == 'disconnect':
-            d = {'room': message['room'],
-                 'text': 'remove_user',
-                 'user': self.user_name}
-            mess = json.dumps(d)
-            all_connections = [con for k in self.connections for con in self.connections[k]]
-            for con in all_connections:
-                con.write_message(mess)
-            del self
-        elif message['text'] == 'change_room':
-            logger.info('pre_change = '+str(self.connections))
-            self.connections[message['new_room']].append(self)
-            logger.info('post_change = '+str(self.connections))
-            self.connections[message['room']].remove(self)  # тут чё та валится постоянно
-            logger.info('post_pop = '+str(self.connections))
-        else:
+        if message['type'] == 'text':
             room = Room.objects.get(title=message['room'])
             message_obj = Message(room=room, username=self.user_name, text=message['text'])
             message_obj.save()
             d = {'room': message['room'],
                  'user': self.user_name,
+                 'type': 'text',
                  'text': censor_message_text(message['text'])}
             mess = json.dumps(d)
-            all_connections = [con for k in self.connections for con in self.connections[k]]
+            all_connections = [con for room in self.connections for con in self.connections[room]]
             logger.info('all_coon = '+str(all_connections))
+            for con in all_connections:
+                con.write_message(mess)
+        elif message['type'] == 'disconnect':
+            d = {'room': message['room'],
+                 'type': 'disconnect',
+                 'user': self.user_name}
+            mess = json.dumps(d)
+            all_connections = [con for room in self.connections for con in self.connections[room]]
+            logger.info('pre_disconnect = '+str(all_connections))
+            for con in all_connections:
+                con.write_message(mess)
+            self.connections[message['room']].remove(self)
+            self.close()
+        elif message['type'] == 'change_room':
+            logger.info('pre_change = '+str(self.connections))
+            self.connections[message['new_room']].append(self)
+            logger.info('post_change = '+str(self.connections))
+            self.connections[message['room']].remove(self)
+            logger.info('post_pop = '+str(self.connections))
+        elif message['type'] == 'edit_room_name':
+            logger
+            try:
+                room = Room.objects.get(title=message['room'])
+                room.title = message['edited_name']
+                room.save()
+                d = {'type': 'edit_room_name',
+                     'room': message['room'],
+                     'edited_name': message['edited_name']}
+                mess = json.dumps(d)
+            except ObjectDoesNotExist:
+                d = {'type': 'error',
+                     'text': 'Такой комнаты не существует'}
+                mess = json.dumps(d)
+
+            all_connections = [con for room in self.connections for con in self.connections[room]]
             for con in all_connections:
                 con.write_message(mess)
 
     def on_message(self, message):
         parsed = tornado.escape.json_decode(message)
-        if parsed['msgtype'] == 'chat':
+        if parsed['msg_from'] == 'chat':
             self.chat_handler(parsed['msg'])
-
-
-#            [ERROR   ] Uncaught exception in /websocket
-#Traceback (most recent call last):
-#  File "C:\Python27\lib\site-packages\tornado\websocket.py", line 303, in wrapper
-#    return callback(*args, **kwargs)
-#  File "D:\SCRIPTS\DJANGO\tornado_chat\chat_app\tornadoapp.py", line 96, in on_message
-#    if parsed['msgtype'] == 'chat':
-#  File "D:\SCRIPTS\DJANGO\tornado_chat\chat_app\tornadoapp.py", line 74, in chat_handler
-#    del self.connections[message['room']][self.user_name]
-#KeyError: u'site_admin'
-#[ERROR   ] Uncaught exception in /websocket
-#Traceback (most recent call last):
-#  File "C:\Python27\lib\site-packages\tornado\websocket.py", line 303, in wrapper
-#    return callback(*args, **kwargs)
-#  File "D:\SCRIPTS\DJANGO\tornado_chat\chat_app\tornadoapp.py", line 96, in on_message
-#    if parsed['msgtype'] == 'chat':
-#  File "D:\SCRIPTS\DJANGO\tornado_chat\chat_app\tornadoapp.py", line 74, in chat_handler
-#    del self.connections[message['room']][self.user_name]
-#KeyError: u'user1'
