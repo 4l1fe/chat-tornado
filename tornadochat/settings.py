bea01bd874233c6f51275abdb7583d7ac11d8ad0
windows <windows@gmail.com>
2013-11-28 23:33:04 +0400

    перенос БД

--- tornadochat/settings.py
+++ tornadochat/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dev.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(PROJECT_PATH, 'dev.sqlite'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
